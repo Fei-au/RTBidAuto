9438b6f6a8a223374cb1256d6bb29bc1d382af7d
fei <fei-ca@outlook.com>
2025-06-06 10:07:30 -0400

    feat: finish most filtering features

--- automation.py
+++ automation.py
@@ -8,6 +8,7 @@ import uuid
 import traceback
 import random
 from playwright.async_api import Page
+import time
 
     
 
@@ -39,6 +40,12 @@ class Automation:
     def set_twenty_switch(self, twenty_switch):
         self.twenty_switch = twenty_switch
         self.show_log(f'Set twenty switch to {self.twenty_switch}')
+        
+    def log_auto_bid(self, func):
+        pass
+    
+    def log_auto_filter(self, func):
+        pass
     
     async def login_bot(self, page, bot_acc, bot_pwd, url):
         # await page.goto(url)
@@ -419,70 +426,143 @@ class Automation:
         else:
             domain = page.url.split('?')[0]
             url = domain + query
-        await page.goto(url)
-        
+        filter_round = 0
         while self.is_filter_running:
-            register_list_tbody = page.get_by_role("rowgroup").locator('id="register-list')
-            trs = register_list_tbody.get_by_role("row")
-            count = await trs.count()
-            for i in range(count):
-                tr = trs.nth(i)
-                # 1. Check bid history total amount
-                total_bid_amount_a = tr.locator('class="lot-bid-history"')
-                ct = await total_bid_amount_a.count()
-                # The bidder hasn't bid yet
-                if ct == 0:
-                    continue
-                total_bid_amount = await total_bid_amount_a.inner_text()
-                try:
-                    float(total_bid_amount[1:-1].replace(',', ''))
-                except Exception as e:
-                    self.show_log(f'Get bidder bid history failed, skip')
-                    continue
-                # 2. If amount is larger than 200, do further investigate
-                declined_flag = False
-                if total_bid_amount > 200:
-                    await total_bid_amount_a.click()
-                    bid_history_modal = page.locator('id="bid-history-modal" modal-content')
-                    bid_history_table = bid_history_modal.get_by_role("table").locator('id="bid-history-table" tbody')
-                    bid_history_trs = bid_history_table.get_by_role('row')
-                    bid_history_trs_count = await bid_history_trs.count()
-                    high_value_bid_count = 0
-                    for j in bid_history_trs_count:
-                        bid_tr = bid_history_trs.nth(j)
-                        bid_status = await bid_tr.locator('class="bid-history-status"').inner_text()
-                        # Only check Accepted items
-                        if bid_status.find("Accepted") != -1:
-                           continue
-                        bid_max = await float(bid_tr.locator('class="bid-history-max-bid"').inner_text())
-                        if bid_max > 200:
-                            high_value_bid_count += 1
-                        else:
-                            continue
-                        if (high_value_bid_count / bid_history_trs_count) >= 0.5:
+            await page.goto(url)
+            round_continue = True
+            start = datetime.now()
+            filter_round += 1
+            while round_continue and self.is_filter_running:
+                await page.wait_for_load_state('networkidle')
+                register_list_container = page.locator('div.register-list-container')
+                register_list_tbody = register_list_container.locator('table#register-list tbody')
+                trs = register_list_tbody.get_by_role("row")
+                count = await trs.count()
+                self.show_log(f'Total trs in this page {count}')
+                for i in range(count):
+                    if not self.is_filter_running:
+                        break
+                    tr = trs.nth(i)
+                    # Bidder id
+                    bidder_id_ele = tr.locator('td.bidder')
+                    bidder_id_ele_a = bidder_id_ele.get_by_role('link').nth(0)
+                    bidder_id_text = await bidder_id_ele_a.inner_text()
+                    bidder_id = bidder_id_text.strip().split(' ')[0]
+                    self.show_log(f"[{bidder_id}] checking...")
+                    # Score
+                    score_text = await tr.locator('td.score a.bidder-profile div').first.inner_text()
+                    score = int(score_text)
+                    if score >= 20:
+                        self.show_log(f'[{bidder_id}] score is equal or larger than 20, skipping')
+                        round_continue = False
+                        break
+                    # Skip processed ids
+                    if bidder_id in self.special_allowed_list or bidder_id in self.already_blocked_list:
+                        continue
+                    # 1. Check bid history total amount
+                    total_bid_amount_a = tr.locator('td.text-center.bids').locator('a[class="lot-bid-history"]')
+                    ct = await total_bid_amount_a.count()
+                    # The bidder's bidding are all pending
+                    if ct == 2:
+                        continue
+                    total_bid_amount_div = total_bid_amount_a.locator('div')
+                    total_bid_amount_div_count = await total_bid_amount_div.count()
+                    # The bidder hasn't bid yet
+                    if total_bid_amount_div_count == 0:
+                        continue
+                    total_bid_amount_text = await total_bid_amount_div.inner_text()
+                    try:
+                        total_bid_amount = float(total_bid_amount_text[1:-1].replace(',', ''))
+                    except Exception as e:
+                        self.show_log(f'Get bidder bid history failed, skip')
+                        continue
+                    # 2. If amount is larger than 200, do further investigate
+                    declined_flag = False
+                    if total_bid_amount > 200:
+                        print(f'total_bid_amount is larger than 200 {total_bid_amount_text}')
+                        self.show_log(f"[{bidder_id}] total bid amount is larger than 200, futher investigating...")
+                        await total_bid_amount_a.click()
+                        bid_history_modal = page.locator('div#bid-history-modal div.modal-content')
+                        await page.wait_for_load_state('networkidle')
+                        await bid_history_modal.wait_for(state='visible')
+                        bid_history_table = bid_history_modal.locator('table#bid-history-table tbody')
+                        await bid_history_table.wait_for(state='visible')
+                        bid_history_trs = bid_history_table.locator('> tr')
+                        bid_history_trs_count = await bid_history_trs.count()
+                        self.show_log(f'[{bidder_id}] total items {bid_history_trs_count}')
+                        high_value_bid_count = 0
+                        winning_bid_count = 0
+                        for j in range(bid_history_trs_count):
+                            bid_tr = bid_history_trs.nth(j)
+                            lot_lead = (await bid_tr.locator('span[class="lot-lead"]').inner_text()).split('-')[0]
+                            # Only check Winning items
+                            bid_status = bid_tr.locator('td[class="bid-history-status hidden-xs text-center"]')
+                            bid_winning_count = await bid_status.locator('div.bid-status-winning:not([class*=" "])').count()
+                            if bid_winning_count == 0:
+                                continue
+                            winning_bid_count += 1
+                            bid_history_max_bid = await bid_tr.locator('td[class="bid-history-max-bid"]').inner_text()
+                            self.show_log(f'[{bidder_id}] [{lot_lead}] bid history max bid {bid_history_max_bid}')
+                            bid_max = float(bid_history_max_bid)
+                            if bid_max > 200:
+                                high_value_bid_count += 1
+                            else:
+                                continue
+                        self.show_log(f'[{bidder_id}] [{lot_lead}] high value wins {high_value_bid_count}, total wins {winning_bid_count}')
+                        if winning_bid_count != 0 and (high_value_bid_count / winning_bid_count) >= 0.5:
                             declined_flag = True
-                            break
-                    # 2.1 Decline items
-                    if declined_flag:
-                        # 2.1 Decline items
-                        for j in bid_history_trs_count:
+                        # 2.1 Half items are over 50%, decline items
+                        if declined_flag:
+                            # 2.1 Decline items
+                            self.show_log(f'[{bidder_id}] items are being declined')
+                            for j in range(bid_history_trs_count):
+                                bid_tr = bid_history_trs.nth(j)
+                                edit_button = bid_tr.locator('button[class="bid-history-edit btn btn-primary"]')
+                                await edit_button.click()
+                                edit_modal = page.locator('div[id="edit-bid-modal"]').locator('div[class="modal-content"]')
+                                await edit_modal.get_by_role('combobox').select_option('3')
+                                edit_modal_footer = edit_modal.locator('[class="modal-footer"]')
+                                await edit_modal_footer.get_by_text('Save').click()
+                                # After click save, the whole page will reload, so wait the network
+                                await page.wait_for_load_state('networkidle')
+                            self.show_log(f'[{bidder_id}] all items are declined')
+                        # 2.2 Bidder is good, pass
+                        else:
                             pass
-                    # 2.2 Bidder pass
+                        # Close the detail modal
+                        await bid_history_modal.get_by_label('Close').click()
+                    # Block the bidder, add to the list        
+                    if declined_flag:
+                        self.show_log(f'[{bidder_id}] profile is being blocked')
+                        await bidder_id_ele_a.click()
+                        await bidder_id_ele.locator('a[class="bidder-profile"]').click()
+                        profile_modal_content = page.locator('div#bidder-profile-modal div.modal-content')
+                        await profile_modal_content.locator('select[name="bidder-profile-decline-reason"]').select_option('7')
+                        await profile_modal_content.locator('button[id="bidder-profile-save"]').click()
+                        profile_saved_modal = page.locator('div[id="bidder-profile-saved-modal"]')
+                        await profile_saved_modal.get_by_label('Close').click()
+                        await profile_modal_content.get_by_label('Close').click()
+                        self.already_blocked_list.append(bidder_id)
+                        self.show_log(f'[{bidder_id}] profile has been blocked')
+                        # Add a log shows that the id has been blocked at which time
                     else:
-                        pass
-                        
-                    
-                        
-
-                    
-                    
-                else:
-                    continue
-                    
-            
-            # If last bidder's reputation is still less than 20, go to next page
-            
-        return "Success"
+                        continue
+                # If last bidder's reputation is still less than 20, go to next page
+                if round_continue and self.is_filter_running:
+                    next_page_button = register_list_container.locator('div#register-list_paginate ul li').get_by_text('Next')
+                    await next_page_button.click()
+            if self.is_filter_running:
+                end = datetime.now()
+                # Every 90 seconds a round
+                time_diff = round((end - start).total_seconds(), 2)
+                if time_diff < 90:
+                    self.show_message(f'Filter bidders round [ {filter_round} ] completed in {time_diff} seconds')
+                    sleep_time = round(90 - time_diff, 2)
+                    self.show_message(f'Waiting for {sleep_time} seconds before next filter round...')
+                    await asyncio.sleep(sleep_time)
+        # Log this transaction
+        # Save special and blocked list locally, add this to this transaction
+        return f"Filter bidders successfully"
     
     async def bot_register_auction(self, page):
         modals = page.locator('app-register-auction')
